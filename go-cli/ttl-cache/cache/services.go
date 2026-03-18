cedd31ed31a9f523ab8d1e269c3fc8e0f0b4cb6c
dhirendraj-cmd <djha5051@gmail.com>
2026-03-18 15:30:31 +0530

    adding set method service

---
+++ go-cli/ttl-cache/cache/services.go
@@ -0,0 +1,50 @@
+package cache
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func (c *LRUCache) Set(key any, val any, ttl int64) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// calculate expiry time
+	expiresAt := time.Now().Unix() + ttl
+
+	// check if key exists
+	if element, ok := c.Items[key]; ok{
+		c.EvictList.MoveToFront(element)
+
+		// update data
+		ent := element.Value.(*EntryCache)
+		ent.value = val
+		ent.expiresAt = expiresAt
+		return
+	}
+
+	// if new key
+	newEntry := &EntryCache{
+		key: key,
+		value: val,
+		expiresAt: expiresAt,
+	}
+	newElement := c.EvictList.PushFront(newEntry)
+	c.Items[key] = newElement
+
+	// check if size > capacity
+	if len(c.Items) > c.Capacity{
+		fmt.Println("Size limmit Exceeded!")
+		lastNode := c.EvictList.Back()
+		if lastNode != nil{
+			// remove from list
+			c.EvictList.Remove(lastNode)
+
+			// remove from map 
+			kv := lastNode.Value.(*EntryCache)
+			delete(c.Items, kv.key)
+		}
+	}
+
+}
