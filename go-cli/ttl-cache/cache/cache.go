87ebef9deaef8ac37407df8d11785dc852512e0a
dhirendraj-cmd <djha5051@gmail.com>
2026-03-22 22:23:30 +0530

    adding heapify logic

--- go-cli/ttl-cache/cache/cache.go
+++ go-cli/ttl-cache/cache/cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"container/heap"
 	"container/list"
 	"sync"
 )
@@ -10,8 +11,54 @@ type EntryCache struct{
 	key any
 	value any
 	expiresAt int64
+	index int
 }
 
+
+// heapify in go
+type ExpiryHeap []*EntryCache
+
+func (eh ExpiryHeap) Len() int { return len(eh)}
+
+// inbuilt min heap logic calling for expiry check
+func (eh ExpiryHeap) Less(i, j int) bool{
+	return eh[i].expiresAt < eh[j].expiresAt
+}
+
+func (eh ExpiryHeap) Swap(i, j int) {
+	eh[i], eh[j] = eh[j], eh[i]
+	eh[i].index = i
+	eh[j].index = j
+}
+
+
+// push to heap
+func (eh *ExpiryHeap) Push(x any){
+	n := len(*eh)
+	item := x.(*EntryCache)
+	item.index = n
+	*eh = append(*eh, item)
+}
+
+
+// pop expired
+func (eh *ExpiryHeap) Pop() any{
+	old := *eh
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	item.index = -1
+	*eh = old[0:n-1]
+	return item
+}
+
+// fix min heap after expiry
+func (eh *ExpiryHeap) update(item *EntryCache, value any, expiresAt int64){
+	item.value = value
+	item.expiresAt = expiresAt
+
+	heap.Fix(eh, item.index)
+}
 
 // management cache using lru patern(may change the name as it is more of a manager than just lruy)
 type LRUCache struct{
@@ -19,13 +66,18 @@ type LRUCache struct{
 	Items map[any]*list.Element
 	mu sync.RWMutex
 	EvictList *list.List
+	Expiration *ExpiryHeap
 }
 
 // constructore for lru
 func NewLRUCache(capacity int) *LRUCache{
+	h := &ExpiryHeap{}
+	heap.Init(h)
 	return &LRUCache{
 		Capacity: capacity,
 		Items: make(map[any]*list.Element),
 		EvictList: list.New(),
+		Expiration: h,
 	}
+
 }
